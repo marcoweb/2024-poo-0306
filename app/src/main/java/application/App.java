315e7a99f8c2dd0faa5c6abc193ca1113860af2f
marcoweb <marcocarvalho.web@gmail.com>
2024-03-27 22:33:38 +0000

    .

--- app/src/main/java/application/App.java
+++ app/src/main/java/application/App.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import application.model.AbstractAtividade;
+import application.model.Compromisso;
 import application.model.Tarefa;
 
 public class App {
@@ -14,5 +19,21 @@ public class App {
         Tarefa t2 = new Tarefa("Segundo Teste");
 
         System.out.println(t2.getDetalhes());
+
+        Compromisso c = new Compromisso();
+        c.setDescricao("Periodo de Avaliações");
+        c.setDataInicial(21, 03, 2024);
+        c.setDataFinal(22, 03, 2024);
+
+        System.out.println(c.getDetalhes());
+
+        List<AbstractAtividade> listaAtividades = new ArrayList<AbstractAtividade>();
+        listaAtividades.add(t);
+        listaAtividades.add(c);
+
+        System.out.println("----------[Lista]----------");
+        for(AbstractAtividade a : listaAtividades) {
+            System.out.println(a.getDetalhes());
+        }
     }
 }
